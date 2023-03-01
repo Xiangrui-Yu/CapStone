197ae95fb91684e449f41698e6fe89d29b28b2ca
Ray Yu <RayYu@Lucky.com>
2023-03-01 01:00:02 -0500

    update

---
+++ app/api/like_routes.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import User, Tweet, Reply, Like, Retweet, db
+from flask_login import current_user, login_user, logout_user, login_required
+from sqlalchemy.orm import joinedload
+
+like_routes = Blueprint('like', __name__)
+
+
+@like_routes.route('/like/<string:object_type>/<int:object_id>', methods =['POST'])
+def like_object (object_type, object_id):
+    object = None
+    like_object = None
+
+    if current_user.is_authenticated:
+
+        if object_type == 'tweets':
+            object = Tweet.query.get(object_id)
+            like_object = Like.query.filter(Like.user_id == current_user.id, Like.tweet_id == object_id).first()
+
+
+        elif object_type == "replies":
+            object = Reply.query.get(object_id)
+            like_object = Like.query.filter(Like.user_id == current_user.id, Like.reply_id == object_id).first()
+
+
+        
+        if not object:
+            return {'errors': [f'{object_type} does not exist']}, 404
+        
+
+        if like_object:
+            db.session.delete(like_object)
+            db.session.commit()
+            
+            if object_type == 'tweets':
+                object.like_count -=1
+            elif object_type =='replies':
+                object.likes_count -=1
+
+            db.session.commit()
+
+            return jsonify({'successfully'})
+        
+        else:
+
+            new_like = Like(user_id = current_user.id, like_count =1)
+
+            if object_type == 'tweets':
+                new_like.tweet_id = object_id
+                object.likes_count +=1
+
+            elif object_type == 'replies':
+                new_like.reply_id = object_id
+                object.likes_count +=1
+            db.session.add(new_like)
+            db.session.commit()
+
+            return jsonify({'successfully'})
+        
+    return {'errors': ['Unauthorized'], "statusCode": 401}, 401
